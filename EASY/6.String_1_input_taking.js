27dad4314085387ece5cad4cac227b91dcd16c58
pappukrs <digitalpappu101@gmail.com>
2024-05-02 03:28:33 +0530

    6.String_1_input_taking

---
+++ EASY/6.String_1_input_taking.js
@@ -0,0 +1,30 @@
+
+function runProgram(input) {
+    // Write code here
+    input = input.split('\n');
+    n = input[0];
+    str = input[1];
+    console.log(str)
+  }
+  
+  if (process.env.USER === "") {
+    runProgram(``);
+  } else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+      read += input;
+    });
+    process.stdin.on("end", function () {
+      read = read.replace(/\n$/, "");
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+    });
+    process.on("SIGINT", function () {
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+      process.exit(0);
+    });
+  }
+  
