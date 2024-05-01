8f4ee895957592dcb634bc7af1c21cab536ce547
pappukrs <digitalpappu101@gmail.com>
2024-05-02 03:04:21 +0530

    decompose the string first approach

--- EASY/5.Decompress_the_String.js
+++ EASY/5.Decompress_the_String.js
@@ -1,3 +1,31 @@
 function decompressString(string) {
-    //write code here
+    let decompressed = '';
+    let currentChar = '';
+    let count = '';
+  
+    for (let i = 0; i < string.length; i++) {
+      if (!isNaN(string[i])) {
+        count += string[i];
+      } else {
+        if (count === '') {
+          if (currentChar !== '') {
+            decompressed += currentChar;
+          }
+          currentChar = string[i];
+        } else {
+          decompressed += currentChar.repeat(parseInt(count));
+          count = '';
+          currentChar = string[i];
+        }
+      }
+    }
+  
+    if (count !== '') {
+      decompressed += currentChar.repeat(parseInt(count));
+    } else {
+      decompressed += currentChar; // Add the current character if no count is given
+    }
+  
+    console.log(decompressed)
   }
+  
