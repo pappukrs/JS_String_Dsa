ce630df18529f91e5772b1f2d9ae032794f6eb15
pappukrs <digitalpappu101@gmail.com>
2024-05-05 09:10:36 +0530

    strings_ii

---
+++ EASY/14.Strings_II.js
@@ -0,0 +1,41 @@
+
+function runProgram(input) {
+    // Write code here
+    input = input.split('\n');
+    testCases = +input[0]
+    let line = 1;
+    for( let i = 1 ;i <= testCases ;i++){
+        let length = input[line];
+        line++;
+        let str = input[line];
+        line++
+        String(str)
+    }
+    
+  }
+  
+  function String(str){
+      console.log(str)
+  }
+  
+  if (process.env.USER === "") {
+    runProgram(``);
+  } else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+      read += input;
+    });
+    process.stdin.on("end", function () {
+      read = read.replace(/\n$/, "");
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+    });
+    process.on("SIGINT", function () {
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+      process.exit(0);
+    });
+  }
+  
