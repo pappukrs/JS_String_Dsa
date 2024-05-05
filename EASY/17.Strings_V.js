e2f3a79a3f69812b19307f4e3dfb4d1eff1a805e
pappukrs <digitalpappu101@gmail.com>
2024-05-05 09:28:23 +0530

    String_V

---
+++ EASY/17.Strings_V.js
@@ -0,0 +1,33 @@
+
+function runProgram(input) {
+    // Write code here
+    
+    input = input.split('\n');
+    let sen = input[0]
+    String(sen)
+  }
+  function String(input){
+      console.log(input)
+  }
+  
+  if (process.env.USER === "") {
+    runProgram(``);
+  } else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+      read += input;
+    });
+    process.stdin.on("end", function () {
+      read = read.replace(/\n$/, "");
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+    });
+    process.on("SIGINT", function () {
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+      process.exit(0);
+    });
+  }
+  
