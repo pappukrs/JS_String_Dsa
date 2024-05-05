6bda07ce27dd778050e57ab2120bc12caf3e6115
pappukrs <digitalpappu101@gmail.com>
2024-05-05 09:15:28 +0530

    string_iii

---
+++ EASY/15.Strings _III.js
@@ -0,0 +1,33 @@
+
+function runProgram(input) {
+    // Write code here
+    input = input.split('\n');
+    let str = input[0]
+
+    String(str)
+  }
+  function String(str){
+    console.log(str)
+  }
+  
+  if (process.env.USER === "") {
+    runProgram(``);
+  } else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+      read += input;
+    });
+    process.stdin.on("end", function () {
+      read = read.replace(/\n$/, "");
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+    });
+    process.on("SIGINT", function () {
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+      process.exit(0);
+    });
+  }
+  
